b26f6f3388d8bb8b05bef44225ff8cdb9a39e830
Ikenna <oguejioforrichard@gmail.com>
2022-03-04 21:07:25 -0800

    object added to file

--- app.js
+++ app.js
@@ -6,5 +6,18 @@ const employees ={
         age: 34,
         contract: '4years',
         position: 'Technical Editor'
+    },
+
+    567:{
+        name: 'Tammy',
+        age: 34,
+        position: 'Sailor',
+        contract: '3 years'
     }
 }
+ function Cars(){
+     this.name = 'Toyota'
+     this.age= 56
+     this.contract = `3 years`
+     this.position = 'Engineer'
+ }
